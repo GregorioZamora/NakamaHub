4d710d93472887d67215a84b1affbf140b992a09
Gregorio Morillo Zamora <gregoriomorilloz@gmail.com>
2026-02-06 12:29:59 +0100

    Add files via upload

---
+++ src/main/java/nakamaHub/HubAniMangaApplication.java
@@ -0,0 +1,13 @@
+package nakamaHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubAniMangaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubAniMangaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nakamaHub/HubAniMangaApplicationTests.java
@@ -0,0 +1,13 @@
+package nakamaHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubAniMangaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
